147ec37420604eac8d0e61d5e8ac0c192ece9180
Henning <henning.zacher@softwarepiloten.de>
2020-02-03 21:59:31 +0100

    init

---
+++ README.md
@@ -0,0 +1,35 @@
+# Utm Parameters to cookies
+
+## Install
+
+```
+  npm install utm-parameters-toolset --save
+  OR
+  yarn add utm-parameters-toolset
+```
+
+## Use
+
+```
+  import { Utm } from 'utm-parameters-toolset'
+  // create utm instance (set domain for allowed domains / subdomain)
+  const utm = new Utm({
+    domain: '.mydomain.com',  // support subdomain
+    expires: 365,
+  })
+  utm.store()  // simply store everything from window.location to cookies
+  utm.get()    // get everything from cookies as object
+```
+
+### License
+
+MIT
+
+[version]: https://img.shields.io/npm/v/exit-intent.svg
+[MIT License]: https://img.shields.io/npm/l/exit-intent.svg
+[Standard]: https://img.shields.io/badge/code%20style-standard-brightgreen.svg
+[Standard Version]: https://img.shields.io/badge/release-standard%20version-brightgreen.svg
+[Size]: https://badges.herokuapp.com/size/npm/exit-intent
+[Size gzip]: https://badges.herokuapp.com/size/npm/exit-intent?gzip=true
+
+Inspired by https://github.com/szymansd/utm-params/blob/master/index.js
---
+++ src/utm-parameters-toolset.js
@@ -0,0 +1,54 @@
+import delve from 'dlv'
+import cookies from 'browser-cookies' // https://www.npmjs.com/package/browser-cookies
+import queryString from 'query-string' // https://www.npmjs.com/package/query-string
+// supported parameters
+const utmParameters = [
+  'utm_source',
+  'utm_medium',
+  'utm_campaign',
+  'utm_content',
+  'utm_name',
+  'utm_term',
+  'referer' // special case
+]
+export class Utm {
+  constructor ({expires = 365, domain = 'localhost'}) {
+    this.expires = expires
+    this.domain = domain
+    this.prefix = 'utm_toolkit_'
+  }
+  static store () {
+    // `window.location`
+    const search = delve(window, 'history.location.search')
+    const searchAsObject = queryString.parse(search)
+    utmParameters.forEach(parameterName => {
+      const foundValue = searchAsObject[parameterName]
+      if (foundValue) {
+        console.log(`found "${foundValue}" for parameterName`)
+        cookies.set(`${this.prefix}${parameterName}`, foundValue, {
+          expires: this.expires,
+          domain: this.domain
+        })
+      }
+    })
+    // referer
+    const referer = delve(window, 'document.referer')
+    if (referer) {
+      console.log(`found "${referer}" for referer`)
+      cookies.set(`${this.prefix}referer`, referer, {
+        expires: this.expires,
+        domain: this.domain
+      })
+    }
+  }
+  static get () {
+    // `window.location`
+    const returnValue = {}
+    utmParameters.forEach(parameterName => {
+      const foundValue = cookies.get(`${this.prefix}${parameterName}`)
+      if (foundValue) {
+        returnValue[parameterName] = foundValue
+      }
+    })
+  }
+}
